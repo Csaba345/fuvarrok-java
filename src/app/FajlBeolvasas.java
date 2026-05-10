f239ac2f4a2ff904732afe4727f64659dae37b23
Csaba345 <csabak491@gmail.com>
2026-05-10 15:21:31 +0200

    faljBeolvasas befejetem

--- src/app/FajlBeolvasas.java
+++ src/app/FajlBeolvasas.java
@@ -6,7 +6,7 @@ package app;
    import java.util.List;
 
 public class FajlBeolvasas {
-    static List<Fuvar>fuvar = new ArrayList<>();
+    static List<Fuvar>fuvarok = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         Path path = Path.of("fuvarrok.csv");
         String egyFuvar = Files.readString(path);
@@ -15,11 +15,29 @@ public class FajlBeolvasas {
         
         
         for(String rend : rendek ){
-            String r[] = rend.split(";");
-            String rsz=r[0];
-            
+            String r[]=rend.split(";");
+            String rsz = r[0];
+            int idomp = Integer.parseInt(r[1]);
+            int osszeg = Integer.parseInt(r[2]);
+            String fitmod = r[3];
+            Fuvar fuvar = new Fuvar(rsz, idomp, osszeg, fitmod);
+            fuvarok.add(fuvar);
         }
+        feladtok();
     
     }
+
+    private static void feladtok() {
+        final int N = fuvarok.size();
+        int i = 0;
+
+        while (i < N && fuvarok.get(i).getFitmod().equals("igen")) {
+            i++;
+        }
+
+        String valasz = i >= N ? "igen" : "nem";
+        System.out.println("Minden fuvar fitmod értéke 'igen': " + valasz);
+        }
+ }
    
-}
+
